eb5ded7c883f46848bb5361a30573ad1d7ff7052
zhangsongbo <zhangp365@gmail.com>
2026-05-29 15:00:46 +0800

    fix: fix a bug

--- server.py
+++ server.py
@@ -921,7 +921,7 @@ class PromptServer():
 
             return web.json_response(job)
 
-        def _get_history_response(self, history):
+        def _get_history_response(history):
             """Build JSON response from prompt history, clearing prompt values."""
             for k in history:
                 history[k]["prompt"] = None
@@ -940,13 +940,13 @@ class PromptServer():
                 offset = -1
 
             history = self.prompt_queue.get_history(max_items=max_items, offset=offset)
-            return self._get_history_response(history)
+            return _get_history_response(history)
 
         @routes.get("/history/{prompt_id}")
         async def get_history_prompt_id(request):
             prompt_id = request.match_info.get("prompt_id", None)
             history = self.prompt_queue.get_history(prompt_id=prompt_id)
-            return self._get_history_response(history)
+            return _get_history_response(history)
 
         @routes.get("/queue")
         async def get_queue(request):
